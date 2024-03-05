73c5e29def5c9185b1eade32845bcca640a1803a
이기영 <rldud112@naver.com>
2024-03-05 17:56:04 +0900

    ✨ feat : crawling test

--- Python/stocker/stocker_news/news_crawling.py
+++ Python/stocker/stocker_news/news_crawling.py
@@ -5,8 +5,9 @@ import time
 # WebDriver 설정
 driver = webdriver.Chrome()
 # 네이버 뉴스 검색 페이지로 이동
-search_query = "삼성전자"
-driver.get(f"https://search.naver.com/search.naver?where=news&query={search_query}")
+search_query = "엘지전자"
+driver.get(f"https://search.naver.com/search.naver?where=news&query={search_query}&pd=9")
+count = 10
 
 # 페이지 로딩 대기
 time.sleep(5)
@@ -16,12 +17,14 @@ html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
 # 뉴스 제목과 링크 추출
-news_items = soup.select('.news_tit')[:10]  # 첫 10개의 뉴스 항목만 선택
+news_items = soup.select('.news_tit')[:count]  # 첫 10개의 뉴스 항목만 선택
+news_des = soup.select('.api_txt_lines dsc_txt_wrap')[:count]
 
 for idx, item in enumerate(news_items, start=1):
     title = item.get_text()  # 뉴스 제목
     link = item['href']  # 뉴스 링크
     print(f"{idx}. {title} - {link}")
+
 
 # WebDriver 종료
 driver.quit()
