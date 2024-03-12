9969a7f1c0a53f06e34cdcdb0a9c257416be1e86
이승현 <tmdgus1761@gmail.com>
2024-03-12 14:29:45 +0900

    ✨ feat : 주식 최근 뉴스 섹션 컴포넌트 생성

---
+++ Frontend/src/components/Stocks/StockNewsSection.tsx
@@ -0,0 +1,12 @@
+import * as s from '@src/components/styles/Stocks/StockNewsSectionStyle'
+
+const StockNewsSection = () => {
+  return (
+    <s.Container>
+      <s.Title>최근 뉴스</s.Title>
+      <s.Wrap>늬우스 리스트</s.Wrap>
+    </s.Container>
+  )
+}
+
+export default StockNewsSection
