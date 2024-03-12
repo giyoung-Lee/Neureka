4c42007e4a1a1c061f70a4975a055709100999ef
이승현 <tmdgus1761@gmail.com>
2024-03-12 14:30:16 +0900

    🎨 style : 주식 최근 뉴스 컴포넌트 style 적용

---
+++ Frontend/src/components/styles/Stocks/StockNewsSectionStyle.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 50vh;
+  gap: 2vh;
+`
+
+export const Title = styled.div`
+  height: 3vh;
+  font-size: 18px;
+  font-weight: bold;
+`
+
+export const Wrap = styled.div`
+  height: 45vh;
+  background-color: bisque;
+`
