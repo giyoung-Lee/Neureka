1290776a4511a7e5fcf835b7a8006a9650e864a2
이승현 <tmdgus1761@gmail.com>
2024-03-12 14:29:22 +0900

    🎨 style : 주식 차트 섹션 컴포넌트 style 적용

---
+++ Frontend/src/components/styles/Stocks/StockChartSectionStyle.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const Info = styled.div`
+  background-color: white;
+  height: 15vh;
+`
+export const InfoTable = styled.table`
+  width: 100%;
+  height: 100%;
+`
+export const TableTd = styled.td`
+  padding: 10px;
+`
+
+export const Price = styled.span`
+  color: var(--color-blue);
+  font-size: 1.7rem;
+  font-weight: 700;
+`
+export const TableTr = styled.tr``
+
+export const Graph = styled.div`
+  margin: 20px 0;
+  width: 100%;
+  height: 60vh;
+  background-color: lightskyblue;
+`
