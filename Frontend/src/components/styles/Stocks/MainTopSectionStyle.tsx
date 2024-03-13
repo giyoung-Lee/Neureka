fba4a314dfe5f1c643c92be2bf44c296af2c30d9
이승현 <tmdgus1761@gmail.com>
2024-03-13 10:43:16 +0900

    🎨 style : 종목 정보 상단바 컴포넌트 style 적용

---
+++ Frontend/src/components/styles/Stocks/MainTopSectionStyle.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: var(--color-lightgrey);
+  padding: 2% 0;
+  border-bottom: 2px solid var(--color-dark);
+  top: 13vh;
+  position: sticky;
+`
+
+export const Title = styled.p`
+  color: var(--color-dark);
+  font-size: 2rem;
+  font-weight: 600;
+`
+
+export const CodeNumber = styled.span`
+  color: var(--color-grey);
+  font-size: 1rem;
+  margin-left: 10px;
+  align-self: flex-end;
+`
