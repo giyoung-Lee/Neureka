a39abc401d6db5c973257681413d4b8cb8209429
이승현 <tmdgus1761@gmail.com>
2024-03-13 10:37:47 +0900

    ✨ feat : 내 종목 조회 컴포넌트 생성

---
+++ Frontend/src/components/Stocks/MyStocksSection.tsx
@@ -0,0 +1,14 @@
+import * as m from '@src/components/styles/Stocks/MyStocksSectionStyle'
+
+const MyStocksSection = () => {
+  return (
+    <m.Wrapper>
+      <m.Favorite>
+        <m.Title>MY STOCK</m.Title>
+        <m.Content></m.Content>
+      </m.Favorite>
+    </m.Wrapper>
+  )
+}
+
+export default MyStocksSection
