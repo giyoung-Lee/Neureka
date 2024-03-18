3345a7820781bbdd6fe9ff0a91bdf8c03d70409d
josuhun <chosh9128@gmail.com>
2024-03-18 15:24:33 +0900

    ✨ feat : 전체 단어 조회 및 검색 필터링 기능

--- Frontend/src/components/Dictionary/LeftSearchSection.tsx
+++ Frontend/src/components/Dictionary/LeftSearchSection.tsx
@@ -1,24 +1,50 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 import * as l from '@src/components/styles/Dictionary/LeftSearchSectionStyle'
-import SearchInput from '@src/common/SearchInput'
 
 import save from '/image/save.png'
 import notsave from '/image/notsave.png'
 import WordCard from './WordCard'
 
+import { Word } from '@src/types/WordType'
+import SearchInput from './SearchInput'
+
 type Props = {}
 
 const LeftSearchSection = (props: Props) => {
   const [search, setSearch] = useState(false)
-  const [question, setQuestion] = useState('')
-  const [isSave, SetIsSave] = useState(false)
+  const [question, setQuestion] = useState<null | string>(null)
+  const [words, SetWords] = useState<null | Word[]>(null)
+  const [originalWords, SetOriginalWords] = useState<null | Word[]>(null)
 
-  const ToggleSave = () => {
-    SetIsSave(!isSave)
-  }
+  // 전체 단어 조회
+  useEffect(() => {
+    axios
+      .get('http://localhost:8080/api/v1/dictionary/get/list')
+      .then(res => {
+        console.log(res.data)
+        SetWords(res.data)
+        SetOriginalWords(res.data)
+        setQuestion('')
+      })
+      .catch(err => console.log(err))
+  }, [])
 
-  const words = Array.from({ length: 10 }, (_, idx) => idx)
+  // 키워드가 제목 또는 내용에 포함된 카드만 조회
+  useEffect(() => {
+    if (question) {
+      const filteredWords = words?.filter(
+        word =>
+          word.title.includes(question) || word.content.includes(question),
+      )
+      if (filteredWords) {
+        SetWords(filteredWords)
+      }
+    } else {
+      SetWords(originalWords)
+    }
+  }, [question])
 
   return (
     <>
@@ -28,27 +54,15 @@ const LeftSearchSection = (props: Props) => {
             <SearchInput
               search={search}
               setSearch={setSearch}
+              question={question}
               setQuestion={setQuestion}
             />
           </l.SearchBar>
-          <l.SearchResult className={search ? `open` : `close`}>
-            <l.SearchTitle>
-              {question}
-              {isSave ? (
-                <l.saveBtn src={save} onClick={ToggleSave} />
-              ) : (
-                <l.saveBtn src={notsave} onClick={ToggleSave} />
-              )}
-            </l.SearchTitle>
-            <l.SearchContent>
-              어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고어쩌고저쩌고쩌고저쩌고
-              어쩌고저쩌고쩌고저쩌고어쩌고저쩌고쩌고저쩌고어쩌고저쩌고 라는 뜻
-            </l.SearchContent>
-          </l.SearchResult>
-          <l.Words className={search ? 'long' : 'short'}>
-            {words.map((it, key) => (
-              <WordCard />
-            ))}
+
+          <l.Words>
+            {words
+              ? words.map((word, idx) => <WordCard word={word} key={idx} />)
+              : null}
           </l.Words>
         </l.Box>
       </l.Wrapper>
