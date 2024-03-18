a4023c95e69daab8d4b491272ce9dbb02b148eec
이승현 <tmdgus1761@gmail.com>
2024-03-18 17:28:16 +0900

    ✨ feat : 선택 종목 상태관리 기능 추가

--- Frontend/src/components/Stocks/MainTopSection.tsx
+++ Frontend/src/components/Stocks/MainTopSection.tsx
@@ -1,10 +1,25 @@
+import { useState } from 'react'
+import { useAtom } from 'jotai'
+import { selectedStockAtom } from '@src/stores/stockAtom'
 import * as s from '@src/components/styles/Stocks/MainTopSectionStyle'
 
 const MainTopSection = () => {
+  const [addMyStock, setAddMyStock] = useState() // my stock 등록
+  const [selectedStock, setSelectedStock] = useAtom(selectedStockAtom) // select 한 종목
+  // seletedStock 이 빈문자열 -> 상태 초기값 설정 필요
+  // usemutation 사용하여 등록 필요
+
+  const handleAddMyStock = () => {}
+
   return (
     <s.Container>
-      <s.Title>삼성전자</s.Title>
-      <s.CodeNumber>(005930)</s.CodeNumber>
+      <s.Title>{selectedStock.name}</s.Title>
+      <s.CodeNumber>({selectedStock.code})</s.CodeNumber>
+      <s.AddButton
+        onClick={() => {
+          handleAddMyStock()
+        }}
+      />
     </s.Container>
   )
 }
