59c335898d6f028ead621e2973b49f98493d2592
이승현 <tmdgus1761@gmail.com>
2024-03-21 16:21:03 +0900

    ✨ feat : 선택한 기업 코드로 파라미터 지정

--- Frontend/src/stores/stockAtom.ts
+++ Frontend/src/stores/stockAtom.ts
@@ -2,7 +2,7 @@ import { atom } from 'jotai'
 import { CompanyType } from '@src/types/CompanyType'
 
 export const selectedCompanyAtom = atom<CompanyType>({
-  companyId: 0,
-  code: '',
-  companyName: '',
+  companyId: 1,
+  code: '0005930',
+  companyName: '삼성전자',
 })
