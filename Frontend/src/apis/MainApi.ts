adbf8cadd627fbad39e4b513d306a1cb63efa03d
윤주찬 <wncks2546@naver.com>
2024-03-25 14:16:51 +0900

    [FE] ✨ feat : 비 로그인 회원 분류 선택 불가능 코드 작성

--- Frontend/src/apis/MainApi.ts
+++ Frontend/src/apis/MainApi.ts
@@ -1,18 +1,12 @@
 import { pythonRequest } from "@src/hooks/pythonMethod";
-import { Category, KeywordCount } from "@src/types/MainType";
+import { publicRequest } from "@src/hooks/requestMethod";
+import { Category } from "@src/types/MainType";
 
 export const fetchKeywords = async (categories: Category[]) => {
   const queryString = categories.map(category => `keywords=${category.name}`).join('&');
-  
-  // `axios`에 전체 URL을 문자열로 전달합니다.
-  const response = await pythonRequest.get(`news/api/bubble/?${queryString}`);
-  return response;
-  
-};
+  return await publicRequest.get(`keyword?${queryString}`);
+}
 
 export const fetchKeywordArticles = async (links: string[]) => {
-  const response = await pythonRequest.post('news/api/keyword_article/', {"links":links})
-  // const response = await pythonRequest.post('news/api/keyword_article/')
-  console.log(response)
-  return response
-}
+  return await pythonRequest.post('news/api/keyword_article/', {"links":links})
+};
--- Frontend/src/components/Main/BubbleCategory.tsx
+++ Frontend/src/components/Main/BubbleCategory.tsx
@@ -1,23 +1,53 @@
 import Category from '@src/components/Main/Category'
 import * as bc from '@src/components/styles/Main/BubbleCategory'
+import { isLoginAtom } from '@src/stores/authAtom'
 import { categoriesAtom, categoryToggleAtom } from '@src/stores/mainAtom'
 import { Categories } from '@src/types/MainType'
 import { useAtom } from 'jotai'
 
 const BubbleCategory = () => {
+  const [isLogin, setIsLogin] = useAtom(isLoginAtom)
   const [categoryToggle, setCategoryToggle] = useAtom(categoryToggleAtom)
   const [selectedCategories, setCategories] = useAtom(categoriesAtom)
   const handleToggleCategory = () => setCategoryToggle(prev => !prev)
 
   return (
-    <>
-      <bc.Container>
-        <bc.ToggleWrapper>
-          <bc.CategoryToggle onClick={handleToggleCategory}>
-            카테고리 선택
-          </bc.CategoryToggle>
-        </bc.ToggleWrapper>
+    <bc.Container>
+      <bc.ToggleWrapper>
+        <bc.CategoryToggle onClick={handleToggleCategory}>
+          카테고리 선택
+        </bc.CategoryToggle>
+      </bc.ToggleWrapper>
 
+      {/* <bc.CategoryWrapper $show={categoryToggle}>
+        {Categories.map((element, key) => (
+          <Category
+            key={key}
+            name={element.name}
+            image={element.image}
+            show={categoryToggle}
+          />
+        ))}
+      </bc.CategoryWrapper>
+
+      <bc.CategoryWrapper $show={categoryToggle}>
+        {selectedCategories.map((element, key) => (
+          <Category
+            key={key}
+            name={element.name}
+            image={element.image}
+            show={categoryToggle}
+          />
+        ))}
+      </bc.CategoryWrapper>
+
+      {!isLogin && (
+        <bc.IsLoginContaier>
+          서비스를 이용하기 위해서 로그인이 필요합니다.
+        </bc.IsLoginContaier>
+      )} */}
+      {/* 로그인 상태에 따라 IsLoginContainer를 조건부로 보여줍니다 */}
+      <bc.CategoryBlocker $isLogin={isLogin}>
         <bc.CategoryWrapper $show={categoryToggle}>
           {Categories.map((element, key) => (
             <Category
@@ -39,8 +69,14 @@ const BubbleCategory = () => {
             />
           ))}
         </bc.CategoryWrapper>
-      </bc.Container>
-    </>
+
+        {!isLogin && (
+          <bc.IsLoginContaier $show={categoryToggle}>
+            서비스를 이용하기 위해서 로그인이 필요합니다.
+          </bc.IsLoginContaier>
+        )}
+      </bc.CategoryBlocker>
+    </bc.Container>
   )
 }
 
