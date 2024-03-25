a5a4cd89283f2c6833ee1f448770f1911bcaab06
이승현 <tmdgus1761@gmail.com>
2024-03-25 16:34:43 +0900

    ✨ feat : 사용자별 최근 조회 기업 조회 및 등록 api 함수 생성

--- Frontend/src/apis/StockApi.ts
+++ Frontend/src/apis/StockApi.ts
@@ -1,6 +1,9 @@
 import { publicRequest } from '@src/hooks/requestMethod'
 import queryString from 'query-string'
-import { CompanyLikeParmasType } from '@src/types/CompanyType'
+import {
+  CompanyLikeParmasType,
+  CompanyLatestParamsType,
+} from '@src/types/CompanyType'
 
 // 기업 전체 조회
 export const fetchCompanyList = async () => {
@@ -27,10 +30,25 @@ export const fetchCompanyLikeList = async (email: string) => {
 }
 
 // 관심 종목 등록
-
 export const fetchCompanyLike = async (params: CompanyLikeParmasType) => {
   return await publicRequest
     .post(`company/like?${queryString.stringify(params)}`)
     .then(res => console.log(res))
+    .catch(err => console.log(err))
+}
+
+// 최근 조회 기업 조회
+export const fetchCompanyLatestList = async (email: string) => {
+  return await publicRequest
+    .get(`company/read?email=${email}`)
+    .then(res => res.data)
+    .catch(err => console.log(err))
+}
+
+// 최근 조회 기업 등록
+export const fetchCompanyLatest = async (params: CompanyLatestParamsType) => {
+  return await publicRequest
+    .post(`company/read?${queryString.stringify(params)}`)
+    .then(res => res.data)
     .catch(err => console.log(err))
 }
