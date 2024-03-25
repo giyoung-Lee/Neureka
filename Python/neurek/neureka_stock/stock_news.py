31aeabce5e11f10e51bb96f49e5a5526f1771417
이기영 <rldud112@naver.com>
2024-03-25 15:23:31 +0900

    🐛 fix : 경로 오류 수정

--- Python/neurek/neureka_stock/stock_news.py
+++ Python/neurek/neureka_stock/stock_news.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
 import requests
 from bs4 import BeautifulSoup, NavigableString, Tag
 from neurek.neureka_stock.models import DetailsArticle
