595108a3cf40fa912f97db1cc0f9be4a03426bfb
josuhun <chosh9128@gmail.com>
2024-03-25 17:24:42 +0900

    🎨 style : 디테일페이지에서 용어사전 검색버튼 추가

--- Frontend/src/containers/styles/NewsDetailContainerStyle.tsx
+++ Frontend/src/containers/styles/NewsDetailContainerStyle.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import SearchIcon from '@mui/icons-material/Search'
 
 export const HeaderImage = styled.div<{ bgimage: string }>`
   height: 60vh;
@@ -12,4 +13,29 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`
+export const GoDictionaryBtn = styled.span`
+  width: 50px;
+  aspect-ratio: 1;
+  border-radius: 20px;
+  position: fixed;
+  right: 3%;
+  bottom: 5%;
+  background-color: var(--color-grey);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(0);
+  transition: transform 250ms ease-in-out;
+  box-shadow: var(--shadow);
+
+  &:hover {
+    transform: translateY(-5%);
+    cursor: pointer;
+  }
+`
+export const Search = styled(SearchIcon)`
+  color: var(--color-lightblue);
+  font-size: 2.3rem !important;
 `
