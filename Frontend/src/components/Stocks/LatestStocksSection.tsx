d10670f6a39c3941459b23e84241e8de3776b683
이승현 <tmdgus1761@gmail.com>
2024-03-25 16:36:24 +0900

    ✨ feat : 최근 조회 기업 prop 전달

--- Frontend/src/components/Stocks/LatestStocksSection.tsx
+++ Frontend/src/components/Stocks/LatestStocksSection.tsx
@@ -1,14 +1,17 @@
+import { CompanyLatestType } from '@src/types/CompanyType'
 import * as l from '@src/components/styles/Stocks/LatestStocksSectionStyle'
 
-const LatestStocksSection = () => {
-  const data: string[] = []
+const LatestStocksSection = (props: { data: CompanyLatestType[] }) => {
+  const { data } = props
 
   return (
     <l.Container>
       <l.Title>최근 조회</l.Title>
       <l.Wrap>
-        {data.length > 0 ? (
-          <l.Item></l.Item>
+        {data && data.length > 0 ? (
+          data.map(item => (
+            <l.Item key={item.crId}>({item.companyCode})</l.Item>
+          ))
         ) : (
           <l.NoneItem>최근 조회한 기업이 없습니다.</l.NoneItem>
         )}
