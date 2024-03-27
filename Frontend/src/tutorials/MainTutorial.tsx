e9b528ff2c2fee86f9144a7282810aec3419fdfd
윤주찬 <wncks2546@naver.com>
2024-03-27 17:48:21 +0900

    [FE] ✨ feat : 메인페이지 카테고리 선택 슬라이드 설정

--- Frontend/src/tutorials/MainTutorial.tsx
+++ Frontend/src/tutorials/MainTutorial.tsx
@@ -9,22 +9,32 @@ const MainTutorial = ({ run }: MainTutorialProps) => {
   const [steps, setSteps] = useState<Step[]>([
     {
       disableBeacon: true,
+      title: '카테고리창 토글',
       target: '.CategoryToggle',
       content: '카테고리 목록 토글할 수 있습니다.',
     },
     {
+      title: '카테고리 선택',
       target: '.CategoryList',
-      content: '가능한 카테고리 목록입니다. 원하는 카테고리를 선택해보세요.',
+      content: '관심있는 카테고리를 선택해보세요.',
     },
     {
+      title: '선택된 카테고리',
       target: '.SelectedCategories',
-      content: '선택된 카테고리가 여기에 표시됩니다.',
+      content: (
+        <>
+          <div>선택된 카테고리가 여기에 표시됩니다.</div>
+          <div>클릭 시 선택된 카테고리를 제거할 수 있습니다.</div>
+        </>
+      ),
     },
     {
+      title: '키워드 버블',
       target: '.BubbleChart',
       content: '선택한 카테고리의 키워드가 나옵니다. 버블을 클릭해보세요.',
     },
     {
+      title: '키워드 기사',
       target: '.KeywordNews',
       content: '클릭한 버블들에 대한 최신 기사를 확인하세요.',
     },
@@ -48,6 +58,11 @@ const MainTutorial = ({ run }: MainTutorialProps) => {
       showSkipButton
       steps={steps}
       callback={handleJoyrideCallback}
+      styles={{
+        options: {
+          zIndex: 10000,
+        },
+      }}
     />
   )
 }
