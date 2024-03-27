96f853b36f92508bcc3d1642cc089ed39bc616e8
josuhun <chosh9128@gmail.com>
2024-03-27 13:49:59 +0900

    ✨ feat : 클라이언트 jwt 설정

--- Frontend/src/stores/authAtom.ts
+++ Frontend/src/stores/authAtom.ts
@@ -2,4 +2,7 @@ import { atom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 
 export const isLoginAtom = atomWithStorage<boolean>('isLogin', false)
- 
+
+export const isAccessTokenAtom = atomWithStorage<string>('accessToken', '')
+export const isRefreshTokenAtom = atomWithStorage<string>('refreshToken', '')
+export const isExpireTimeAtom = atomWithStorage<number>('tokenExpireTime', 0)
