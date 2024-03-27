0744e37c2d7c697e730a5ae11bdb90bf4d3e0daa
josuhun <chosh9128@gmail.com>
2024-03-27 13:58:30 +0900

    🧪 test :  requestMethod token관리 수정

--- Frontend/src/hooks/requestMethod.ts
+++ Frontend/src/hooks/requestMethod.ts
@@ -10,36 +10,36 @@ export const publicRequest = axios.create({
 
 const accessToken = localStorage.getItem('accessToken')
 
-publicRequest.interceptors.request.use(
-  config => {
-    if (!config.headers.Authorization) {
-      config.headers['Authorization'] = accessToken
-    }
-    return config
-  },
-  error => {
-    console.log(error)
-    return Promise.reject(error)
-  },
-)
+// publicRequest.interceptors.request.use(
+//   config => {
+//     if (!config.headers.Authorization) {
+//       config.headers['Authorization'] = accessToken
+//     }
+//     return config
+//   },
+//   error => {
+//     console.log(error)
+//     return Promise.reject(error)
+//   },
+// )
 
-publicRequest.interceptors.response.use(
-  response => {
-    return response
-  },
-  async error => {
-    if (error.response?.status === 403) {
-      console.log('토큰 없음')
-      console.log(error)
+// publicRequest.interceptors.response.use(
+//   response => {
+//     return response
+//   },
+//   async error => {
+//     if (error.response?.status === 403) {
+//       console.log('토큰 없음')
+//       console.log(error)
 
-      error.config.headers = {
-        Authorization: accessToken,
-      }
-    } else if (error.response?.status === 401) {
-      console.log('토큰 만료')
-      console.log(error)
-    } else {
-      return error
-    }
-  },
-)
+//       error.config.headers = {
+//         Authorization: accessToken,
+//       }
+//     } else if (error.response?.status === 401) {
+//       console.log('토큰 만료')
+//       console.log(error)
+//     } else {
+//       return error
+//     }
+//   },
+// )
