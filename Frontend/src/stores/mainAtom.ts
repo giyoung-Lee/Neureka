287f2a8950fdfbe9185f58627bc30af4f712f321
윤주찬 <wncks2546@naver.com>
2024-03-28 16:49:55 +0900

    [FE] 🐛 fix : 응답데이터 및 관련 상태관리, react query 코드 수정

--- Frontend/src/stores/mainAtom.ts
+++ Frontend/src/stores/mainAtom.ts
@@ -4,4 +4,4 @@ import { atom } from "jotai"
 export const categoryToggleAtom = atom(true)
 export const categoriesAtom = atom<Category[]>([])
 export const keywordsAtom = atom<KeywordCount[]>([])
-export const selectedKeywordAtom = atom<KeywordCount>({keyword: '', count: 0, links: []})
+export const selectedKeywordAtom = atom<KeywordCount>({keyword: '', count: 0, ids: []})
