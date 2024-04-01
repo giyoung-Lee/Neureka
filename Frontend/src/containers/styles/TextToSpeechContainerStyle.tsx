a94b3922431e7ecf7e88ad69a12481216291c215
이승현 <tmdgus1761@gmail.com>
2024-04-01 15:11:35 +0900

    🎨 style : tts 최상위 컴포넌트 크기 설정

--- Frontend/src/containers/styles/TextToSpeechContainerStyle.tsx
+++ Frontend/src/containers/styles/TextToSpeechContainerStyle.tsx
@@ -1,3 +1,24 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30%;
+  height: auto;
+`
+
+export const Title = styled.div`
+  font-size: 1.4rem;
+  font-weight: 600;
+  margin-bottom: 10%;
+`
+
+export const Setting = styled.div`
+  margin-bottom: 8%;
+  display: flex;
+  flex-direction: column;
+
+  > * {
+    margin-bottom: 5%;
+  }
+`
