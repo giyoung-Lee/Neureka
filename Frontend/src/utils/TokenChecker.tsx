2e3d5818a8dc21548f39df89870be4432e833e40
김유산 <rladbtks@naver.com>
2024-04-02 13:46:23 +0900

    ✨ feat : reissue 수정

--- Frontend/src/utils/TokenChecker.tsx
+++ Frontend/src/utils/TokenChecker.tsx
@@ -27,7 +27,7 @@ const TokenChecker = (props: Props) => {
 
   const refresh = async () => {
     const res = await axios.post(
-      `${import.meta.env.VITE_REACT_APP_BASE_URL}/api/v1/reissue`,
+      `http://j10c105.p.ssafy.io/api/v1/reissue`,
     )
 
     const now = new Date().getTime()
